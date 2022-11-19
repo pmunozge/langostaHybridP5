76cb3b519489a4b041de6b9973e6df125ab6cc02
spozomeza <spozomeza@uoc.edu>
2022-11-20 00:21:51 +0100

    Se agrega detalle menu Nuevo Rt4eto en las pantallas requeridas

---
+++ app/widgets/MenuRetos.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, StyleSheet,TouchableOpacity, Text, Alert} from 'react-native';
+import {styles} from '../estilosApp.js';
+import { withNavigation } from 'react-navigation';
+
+
+
+
+
+export class MenuRetos extends React.Component{
+
+    
+
+    viewMsg = () => {
+        Alert.alert(
+            "Botón presionado",
+            "Langosta.hibryd",
+            [
+                {
+                    text: "Si",
+                    
+                },
+            ],
+             )
+    }
+   render(){
+ 
+        return(
+            <View style={styles.contenedor}>
+                <View style={styles.filareto}>
+                    <TouchableOpacity style={styles.botonreto} onPress={() => this.props.navigate('NuevoReto')}>
+                        <Text style={styles.textoBoton}>NUEVO RETO</Text>
+                    </TouchableOpacity>
+            
+                </View>
+             
+                <View style={styles.fila}>
+                
+                </View>
+               
+        </View>
+        )
+    }
+
+}
